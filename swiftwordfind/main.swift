c17d319515cb0a1c62aa53a9310fd4a39f09228a
NZjeux26 <91103932+NZjeux26@users.noreply.github.com>
2024-11-09 13:43:27 +1300

    Initial Commit

--- swiftwordfind/main.swift
+++ swiftwordfind/main.swift
@@ -7,5 +7,15 @@
 
 import Foundation
 
-print("Hello, World!")
-
+if let path = Bundle.main.path(forResource: "word_alpha", ofType: "txt") { // conbines an if with the file path
+    do {
+        let content = try String(contentsOfFile: path, encoding: .utf8)
+        let lines = content.components(separatedBy: .newlines)
+        
+        for line in lines {
+            print(line)
+        }
+    } catch {
+        print("Error no file found: \(error)")
+    }
+}
